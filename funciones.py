58b6de1fbc53384fd2f489b790f45d8d974a9ead
Yonatan <yonatanyonyonatan@gmail.com>
2025-08-09 08:37:12 -0500

    Se agrego el archivo funciones.py

---
+++ funciones.py
@@ -0,0 +1,10 @@
+#Finciones
+#Evitan repetir código
+#Reutilizar procesos comunes
+#Organiza mejor el código
+#Facil para leer y entender el código
+
+
+#Tipos de funciones
+def mostarMensaje(numero1,numero2):
+ print("")
